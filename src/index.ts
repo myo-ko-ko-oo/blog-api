05e9a87841c9e978062df185091f841dc2e0f838
MYO KO KO OO <myokokooo.5287@gmail.com>
2025-09-20 18:01:05 +0630

    fix src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { app } from "./app";
 import express, { Request, Response, NextFunction } from "express";
 
-const PORT = process.env.PORT || 4000;
-// const host = "0.0.0.0";
+// const PORT = process.env.PORT || 4000;
+// // const host = "0.0.0.0";
 
-app.listen(Number(PORT), () => console.log(`Server is running at ${PORT}:`));
+// app.listen(Number(PORT), () => console.log(`Server is running at ${PORT}:`));
